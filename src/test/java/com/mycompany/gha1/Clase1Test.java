734f8d32186e76b5cb71337eae1f5106d65085f6
Danilo Juvinao <proyectos@ibcaribe.com>
2021-10-26 22:45:19 -0500

    primer commit

---
+++ src/test/java/com/mycompany/gha1/Clase1Test.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gha1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author juvinao
+ */
+public class Clase1Test {
+    
+    public Clase1Test() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sumar method, of class Clase1.
+     */
+    @Test
+    public void testSumar() {
+        System.out.println("sumar");
+        Clase1 instance = new Clase1();
+        int result = instance.sumar(4, 5);
+        assertEquals(9, result);
+    }
+    
+}
